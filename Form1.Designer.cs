9fc2b74daaa3ba12716c8bbaccec896843064757
duyhoang1513-source <duyhoang1513@gmail.com>
2026-04-20 21:04:07 +0700

    Create Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,31 @@
+namespace PresentationSnapshot
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.Text = "Presentation Snapshot Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            
+            this.ResumeLayout(false);
+        }
+    }
+}
